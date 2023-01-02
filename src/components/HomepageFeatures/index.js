708b9769613e0917e696db3942709f483471f913
Alan <alan@MacBook-Pro.lan>
2023-01-02 15:59:49 -0600

    inital commit

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Context',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+      I speak to your technical experts and map out the story.
+
+      </>
+    ), 
+},
+{
+    title: 'Interviews',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        I speak with your technical experts and map out the story's narrative.
+        
+      </>
+    ),
+  },
+  {
+    title: 'Writing & Editing',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        I use 2-3 rounds of <code>editing</code> to get the story ready for publishing. 
+
+        </>
+    
+        ), 
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/markdown-page.md
@@ -0,0 +1,11 @@
+---
+title: Markdown page example
+---
+
+# Markdown page example
+
+You don't need React to write simple standalone pages.
+
+## Content structure 
+
+Part of me thinks, I'm getting a bit lost, but I beleive that I'm on track. 
