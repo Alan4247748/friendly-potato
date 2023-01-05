16e2b19e576853dca74a00df5bc35b33adc6b8dc
Alan4247748 <murphy@pbranding.de>
2023-01-05 11:47:32 -0600

    "custom font and react page component"

--- src/pages/my-react-page.js
+++ src/pages/my-react-page.js
@@ -7,6 +7,24 @@ export default function MyReactPage() {
     <Layout>
       <h1>My React page</h1>
       <p>This is a React page</p>
-    </Layout>
-  );
+<div class="card-demo">
+  <div class="card">
+    <div class="card__header">
+      <h3>Lorem Ipsum</h3>
+    </div>
+    <div class="card__body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+        suspendisse ultrices gravida.
+      </p>
+    </div>
+    <div class="card__footer">
+      <button class="button button--secondary button--block">See All</button>
+    </div>
+  </div>
+</div>
+</Layout>
+);
+
 }
