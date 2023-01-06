4d5988b5281e8ce0413111451850b204f2606945
Alan4247748 <murphy@pbranding.de>
2023-01-06 16:45:52 -0600

    "stuff"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
         title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/newlogo.svg',
         },
         items: [
           {
