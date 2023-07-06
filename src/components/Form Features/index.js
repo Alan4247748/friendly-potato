7dac7e1ca17da2e7435a0e5969525d51c94eceb8
Alan4247748 <murphy@pbranding.de>
2023-07-06 19:13:19 +0200

    "form issues never ending"

--- src/components/Form Features/index.js
+++ src/components/Form Features/index.js
@@ -23,10 +23,10 @@ const Form = () => {
     setMessage('');
 
     // Submit the form
-    const form = e.target;
-    fetch(form.action, {
+    fetch('/', {
       method: 'POST',
-      body: new FormData(form),
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encodeFormData(formData),
     })
       .then((response) => {
         console.log('Form submitted successfully:', response);
@@ -35,7 +35,7 @@ const Form = () => {
         // Redirect the user to the success page after a delay
         setTimeout(() => {
           window.location.href = '/success'; // Replace '/success' with the actual URL of your success page
-        }, 100); // Delay of 2000 milliseconds (2 seconds) before redirecting
+        }, 2000); // Delay of 2000 milliseconds (2 seconds) before redirecting
       })
       .catch((error) => {
         console.error('Error submitting form:', error);
@@ -45,12 +45,30 @@ const Form = () => {
     console.log('Form submitted!');
   };
 
+  const encodeFormData = (data) => {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
+  };
+
   return (
     <div>
       <h1>Contact Me</h1>
-      <form className="form" onSubmit={handleSubmit} name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
-        {/* ...form fields... */}
-        <button type="submit">Click Me</button>
+      <form className="form" onSubmit={handleSubmit} name="contact" method="post" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </p>
+        <p>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </p>
+        <p>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+        </p>
+        <button type="submit">Submit</button>
       </form>
       <p className={styles.emailText}>
         Email: <span className={styles.emailAddress}>Donohomurphy@gmail.com</span>
