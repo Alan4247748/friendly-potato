fbf0956480531450966a4ce86cb488dbe2305a74
Alan4247748 <murphy@pbranding.de>
2023-07-06 12:53:23 +0200

    "sidebar & success page"

--- src/components/Form Features/index.js
+++ src/components/Form Features/index.js
@@ -5,6 +5,7 @@ const Form = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,6 +31,11 @@ const Form = () => {
       .then((response) => {
         console.log('Form submitted successfully:', response);
         // Handle success scenario
+        setIsSubmitted(true); // Set the state to indicate the form submission is successful
+        // Redirect the user to the success page after a delay
+        setTimeout(() => {
+          window.location.href = '/success'; // Replace '/success' with the actual URL of your success page
+        }, 2000); // Delay of 2000 milliseconds (2 seconds) before redirecting
       })
       .catch((error) => {
         console.error('Error submitting form:', error);
@@ -39,37 +45,22 @@ const Form = () => {
     console.log('Form submitted!');
   };
 
+  if (isSubmitted) {
+    // Render a success message or component instead of the form
+    return (
+      <div>
+        <h1>Form Submitted Successfully!</h1>
+        <p>Thank you for your submission.</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Contact Me</h1>
       <form className="form" onSubmit={handleSubmit} name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <textarea
-          name="message"
-          placeholder="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
+        {/* ...form fields... */}
         <button type="submit">Click Me</button>
-        <p className={styles.hidden}>
-          <label>
-            Don’t fill this out if you’re human: <input name="bot-field" />
-          </label>
-        </p>
       </form>
       <p className={styles.emailText}>
         Email: <span className={styles.emailAddress}>Donohomurphy@gmail.com</span>
---
+++ src/pages/Success.md
@@ -0,0 +1,11 @@
+---
+title: Legal
+---
+
+
+# Contact Form Submitted Successfully!
+
+Thank you for reaching out!
+I will get back to you ASAP.
+
+[Home Page](index.js)
