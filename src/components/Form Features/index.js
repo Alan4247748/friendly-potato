3743357b3d6f954d806a477aa24aeec150a63cfe
Alan4247748 <murphy@pbranding.de>
2023-07-06 17:43:16 +0200

    "deleted if statement"

--- src/components/Form Features/index.js
+++ src/components/Form Features/index.js
@@ -17,14 +17,6 @@ const Form = () => {
     };
     // You can perform additional validation or modify the form data as needed
 
-    // Email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      console.error('Invalid email format');
-      // Handle error scenario for invalid email format
-      return; // Stop the submission process
-    }
-
     // Reset the form fields
     setName('');
     setEmail('');
@@ -40,8 +32,10 @@ const Form = () => {
         console.log('Form submitted successfully:', response);
         // Handle success scenario
         setIsSubmitted(true); // Set the state to indicate the form submission is successful
-        // Redirect the user to the success page immediately
-        window.location.href = '/success'; // Replace '/success' with the actual URL of your success page
+        // Redirect the user to the success page after a delay
+        setTimeout(() => {
+          window.location.href = '/success'; // Replace '/success' with the actual URL of your success page
+        }, 100); // Delay of 2000 milliseconds (2 seconds) before redirecting
       })
       .catch((error) => {
         console.error('Error submitting form:', error);
@@ -51,19 +45,12 @@ const Form = () => {
     console.log('Form submitted!');
   };
 
-  if (isSubmitted) {
-    // Render a success message or component instead of the form
-    return null; // Return null to hide any content
-  }
-
   return (
     <div>
       <h1>Contact Me</h1>
       <form className="form" onSubmit={handleSubmit} name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
-        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-        <button type="submit">Submit</button>
+        {/* ...form fields... */}
+        <button type="submit">Click Me</button>
       </form>
       <p className={styles.emailText}>
         Email: <span className={styles.emailAddress}>Donohomurphy@gmail.com</span>
