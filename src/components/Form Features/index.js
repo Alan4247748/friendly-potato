a9649af9a1e6ef46c5743d1df303be630c55099c
Alan4247748 <murphy@pbranding.de>
2023-07-07 11:48:12 +0200

    "commits"

--- src/components/Form Features/index.js
+++ src/components/Form Features/index.js
@@ -2,45 +2,47 @@ import React, { useState } from 'react';
 import styles from './styles.module.css';
 
 const Form = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
 
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Perform form submission logic here
-    const formData = {
-      name: name,
-      email: email,
-      message: message,
-    };
-    // You can perform additional validation or modify the form data as needed
+
+    try {
+      // Submit the form
+      const response = await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encodeFormData(formData)
+      });
+
+      console.log('Form submitted successfully:', response);
+      // Handle success scenario
+      setIsSubmitted(true); // Set the state to indicate the form submission is successful
+      // Redirect the user to the success page
+      window.location.href = '/success'; // Replace '/success' with the actual URL of your success page
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      // Handle error scenario
+      window.location.href = 'https://www.damurphy.com/404';
+    }
 
     // Reset the form fields
-    setName('');
-    setEmail('');
-    setMessage('');
-
-    // Submit the form
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encodeFormData(formData),
-    })
-      .then((response) => {
-        console.log('Form submitted successfully:', response);
-        // Handle success scenario
-        setIsSubmitted(true); // Set the state to indicate the form submission is successful
-        // Redirect the user to the success page after a delay
-        setTimeout(() => {
-          window.location.href = '/success'; // Replace '/success' with the actual URL of your success page
-        }, 2000); // Delay of 2000 milliseconds (2 seconds) before redirecting
-      })
-      .catch((error) => {
-        console.error('Error submitting form:', error);
-        // Handle error scenario
-      });
+    setFormData({
+      name: '',
+      email: '',
+      message: ''
+    });
 
     console.log('Form submitted!');
   };
@@ -54,19 +56,19 @@ const Form = () => {
   return (
     <div>
       <h1>Contact Me</h1>
-      <form className="form" onSubmit={handleSubmit} name="contact" method="post" data-netlify="true">
+      <form className={styles.form} onSubmit={handleSubmit} name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
         </p>
         <p>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
         </p>
         <p>
           <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+          <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
         </p>
         <button type="submit">Submit</button>
       </form>
